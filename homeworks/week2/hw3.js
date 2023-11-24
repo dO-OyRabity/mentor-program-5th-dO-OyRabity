300ff1f5ced2e2ad40a71c8b5dd74482689c8fb2
dO-OyRabity <wantingmielhughug@gmail.com>
2023-11-24 14:10:46 +0800

    finished week2

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,10 @@
 function reverse(str) {
-  
+  var result = ''
+  var count = str.length
+  for(var i = count-1 ; i>=0 ; i--){
+    result+=str[i]
+  }
+  console.log(result)
 }
 
 reverse('hello');
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,10 +1,23 @@
 function join(arr, concatStr) {
-  
+  var result = ''
+  for (var i = 0; i <= arr.length * 2 - 2; i++) {
+    if (i % 2 === 0) {
+      result += arr[i / 2]
+      console.log(i)
+    } else {
+      result += concatStr
+    }
+  }
+  return result
 }
 
 function repeat(str, times) {
-  
+  var result = ''
+  for (var i = 1; i <= times; i++) {
+    result += str
+  }
+  return result
 }
 
-console.log(join(['a'], '!'));
-console.log(repeat('a', 5));
+console.log(join(['a', 'c', 'd', 'e', 'f', 'g'], '!'))
+console.log(repeat('a', 5))
